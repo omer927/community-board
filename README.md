6f41f1ad092af9ff9f8028d50eafe406fd1ba9de
Omer Omer <omersalim2006@gmail.com>
2026-06-08 01:23:37 -0400

    feat:default

--- README.md
+++ README.md
@@ -4,26 +4,27 @@ Submitted by: **Omer Omer**
 
 This web app: **Is a hub for gamers to find local tournaments, game release dates, and multiplayer LFG (Looking-For-Group) communities.**
 
-Time spent: **X** hours spent in total
+Time spent: **~2** hours spent in total
+
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-- [ ] **The app has a cohesive, unique theme for events or resources relevant to a specific community**
-  - [ ] Header/title describing the theme is displayed
-- [ ] **At least 10 unique events or resources are displayed in a responsive card format**
-  - [ ] There are at least 10 cards displayed for 10 different events 
-  - [ ] The cards should be displayed in an organized format (ex. a grid, or in one line)
-  - [ ] Each card should include some information about the event or resource
+- [x] **The app has a cohesive, unique theme for events or resources relevant to a specific community**
+  - [x] Header/title describing the theme is displayed
+- [x] **At least 10 unique events or resources are displayed in a responsive card format**
+  - [x] There are at least 10 cards displayed for 10 different events 
+  - [x] The cards should be displayed in an organized format (ex. a grid, or in one line)
+  - [x] Each card should include some information about the event or resource
 
 
 The following **optional** features are implemented:
 
-- [ ] Buttons or links to a related resources are on each card component
-  - [ ] All cards have buttons or links in addition to text
-- [ ] The site is responsive for both desktop and mobile formats
-  - [ ] Web app is shown in a mobile format
+- [x] Buttons or links to a related resources are on each card component
+  - [x] All cards have buttons or links in addition to text
+- [x] The site is responsive for both desktop and mobile formats
+  - [x] Web app is shown in a mobile format
   - [ ] **Video Walkthrough Special Instructions**: To ease the grading process, please use Chrome Developer Tools' "Toggle Device" button to demonstrate that your web application's responsiveness in both a desktop *and* a mobile format. Detailed instructions can be found below this stretch feature on the project page. 
 
 The following **additional** features are implemented:
--- community-board/src/components/Container.jsx
+++ community-board/src/components/Container.jsx
@@ -5,85 +5,85 @@ const Container = () => {
     return(
         <div className="Container">
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/maxresdefault.jpg"/>
+                <Events events="Rocket League 3v3 Open Tournament" location="The Grid Arena"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div>
             <div className="card" >
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/Among_Us_cover_art"/>
+                <Events events="Among Us" location=""/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div>
             <div className="card" >
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/images.jpeg"/>
+                <Events events="Gartic Phone" location="The Artisan Perk"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div>
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/minecraft.webp"/>
+                <Events events="Minecraft Creative Build-Off" location="Oak Ridge Community Center"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div>
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/league-of-legends.webp"/>
+                <Events events="League of Legends" location="Copperwood Public Library"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div> 
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/valorant-offwhitelaunch-keyart.jpg"/>
+                <Events events="Valorant Tactical Night" location="Nexus Cyber Cafe"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div>
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/the-future-of-fortnite-is-here-1920x1080-fd281a65c8ef.jpg"/>
+                <Events events="Fortnite Zero Build Scrims" location="The Victory Lounge"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div>
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/images (1).jpeg"/>
+                <Events events="Mario Kart 8 Deluxe Grand Prix" location="Pinecrest Rec Center"/>
                <a href="">
                     <button>View Menu</button>
                 </a>
             </div> 
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/images (2).jpeg"/>
+                <Events events="Monster Hunter: Wilds Co-Op Hunt" location="Whispering Pines Tavern"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div> 
             <div className="card"> 
-                <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <img src="/images (3).jpeg"/>
+                <Events events="Call of Duty: Warzone Squads Drop" location="The Armory Combat Zone"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div>
             <div className="card"> 
                 <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <Events events="Sea of Thieves Pirate Crew Voyage" location="The Salty Sailor Tavern"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
             </div> 
             <div className="card"> 
                 <img src="/wireframe-box-270x2031 (1).jpg"/>
-                <Events events="Fancy Dinner 🎩" location=""/>
+                <Events events="Fall Guys Ultimate Knockout Sprint" location="The Blunderdome Rec Center"/>
                 <a href="">
                     <button>View Menu</button>
                 </a>
